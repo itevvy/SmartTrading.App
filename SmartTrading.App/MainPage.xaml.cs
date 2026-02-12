304b49494f7bc35344f8009abdc12a948a883eef
itevvy <revival6@naver.com>
2026-02-12 22:30:20 +0900

    초기 커밋.

---
+++ SmartTrading.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartTrading.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
